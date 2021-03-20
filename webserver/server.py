db2c04c1a3d6237f24f5811b90b6aa9361502dbc
Maria Teresa Tome <55467678+mteretome@users.noreply.github.com>
2021-03-20 14:39:23 -0400

    small fix

--- webserver/server.py
+++ webserver/server.py
@@ -165,7 +165,7 @@ def ingredients():
    if request.method=='POST':
       input=request.form.get('search-box')
       input=input.strip().lower()
-      cursor = g.conn.execute("select distinct i.ingredient_name, e.episode_name, e.air_date, c.is_entree, c.is_appetizer, c.is_dessert FROM ingredient i,meal_contains RIGHT JOIN cook ON meal_contains.course_id=cook.course_id RIGHT JOIN course c ON meal_contains.course_id=c.course_id RIGHT JOIN episode e ON e.series_episode=cook.series_episode WHERE i.ingredient_name LIKE '%%s%'"%input)
+      cursor = g.conn.execute("select distinct i.ingredient_name, e.episode_name, e.air_date, c.is_entree, c.is_appetizer, c.is_dessert FROM ingredient i,meal_contains RIGHT JOIN cook ON meal_contains.course_id=cook.course_id RIGHT JOIN course c ON meal_contains.course_id=c.course_id RIGHT JOIN episode e ON e.series_episode=cook.series_episode WHERE i.ingredient_name LIKE '%s%'"%input)
       ings  = [("ingredient_name","episode_name","air_date", "is_entree","is_appetizer", "is_dessert")]
       for result in cursor:
          ings.append(result)  # can also be accessed using result[0]
